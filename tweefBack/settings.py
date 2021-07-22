f01bf62ea1e7bcbde7931f4b5ff9fb777e6bc096
emrz98 <emrz98@gmail.com>
2021-07-21 20:12:25 -0500

    signup start

--- tweefBack/settings.py
+++ tweefBack/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tweef_app'
+    'tweef_app',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
